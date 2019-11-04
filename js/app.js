27ee854020c0abdd22c0abe1b855189d935777e3
Mow69 <mouazmouaz@hotmail.fr>
2019-11-04 18:08:39 +0100

    css in progress

--- js/app.js
+++ js/app.js
@@ -32,7 +32,7 @@ function init() {
             let newDiv = document.createElement("div");
             let newContent = document.createTextNode(item['Title']);
             newDiv.appendChild(newContent);
-            var img = new Image();
+            let img = new Image();
             img.src = item['Poster'];
             newDiv.appendChild(img);
             currentDiv.append(newDiv);
@@ -41,7 +41,7 @@ function init() {
 }
 
 function callApi(type, url, callback) {
-    var http = new XMLHttpRequest();
+    let http = new XMLHttpRequest();
     http.open(type, url, true);
 
     http.setRequestHeader('Content-type', 'application/json');
@@ -57,6 +57,84 @@ function callApi(type, url, callback) {
     http.send();
 }
 
+
+
+
+// TODO: REFACTORISER CETTE PARTIE
+//  (penser qu'il est possible en JS d'écouter du html, afin de créer une méthode
+//  de refacto dynamique des pages et de la valeur du display)
+
+
+
+// let pageArray = [0, 1, 2, 3, 4, 5];
+//
+// let titleArray = ['Landing page', 'Movies page', 'Votes page', 'Profil page', 'Settings page', 'About us']
+//
+// const Router = {
+//     page: pageArray,
+//     title: titleArray,
+//     styleDisplay : "",
+// };
+//
+// function goTo(page) {
+//     Router.page = page;
+//     switch (page) {
+//         case 0:
+//             Router.title = '#page-1';
+//             styleDisplay = "";
+//             console.log();
+//             break;
+//
+//         case 1:
+//             Router.title = '#page-2';
+//             styleDisplay = "";
+//             console.log();
+//
+//             break;
+//         case 2:
+//             Router.title = '#page-3';
+//             styleDisplay = "";
+//             console.log();
+//
+//
+//             break;
+//         case 3:
+//             Router.title = '#page-4';
+//             styleDisplay = "";
+//             console.log();
+//
+//             break;
+//         case 4:
+//             Router.title = '#page-5';
+//             styleDisplay = "";
+//             console.log();
+//
+//             break;
+//         case 5:
+//             Router.title = '#page-6';
+//             styleDisplay = "";
+//             console.log();
+//
+//             break;
+//         default:
+//             throw "Page not found"
+//     }
+// }
+//
+// function renderTitle() {
+//     document.querySelector('#title').innerHTML = Router.title
+// }
+//
+// function renderStyleDisplay() {
+//     document.querySelector('#styleDisplay').innerHTML = Router.styleDisplay
+// }
+
+
+
+
+// function showPage1(page, displayConf)
+
+
 function showPage1() {
     document.querySelector("#page-1").style.display = "";
     document.querySelector("#page-2").style.display = "none";
@@ -64,7 +142,15 @@ function showPage1() {
     document.querySelector("#page-4").style.display = "none";
     document.querySelector("#page-5").style.display = "none";
     document.querySelector("#page-6").style.display = "none";
+
+    // let page = document.addEventListener("click", );
+    // let displayConf = ;
+
+    // document.getElementById({page}).style.display = {displayConf};
+    // let x = document.querySelector(`${page}`).style.display = '${displayConf}';
+
 }
+
 
 function showPage2() {
     document.querySelector("#page-1").style.display = "none";
